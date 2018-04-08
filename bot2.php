2ead09c6f03c15c4b76a20623dea30b430f5cf49
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2018-04-09 04:06:19 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -7,8 +7,8 @@ Support by : Mastah Ervan
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
 
-$channelAccessToken = 'wp9mcz26A/rE05L98V+T7Pe/YE9A+PFEjw1Gr6X/Jstzi2V39ggocyDkbKFWcCJXY9DwmDwdX1aMwUSsCYWQiFXncDPzoCCLVegj9KDQTyKP+gAv5caga1iaDEHV0GKTBrnbLaCBZqUbeq6fMmAlHwdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-$channelSecret = 'c5016e837fd8041cccdc1d1454f349b7';//sesuaikan
+$channelAccessToken = 'CkHhBC6Ambq9d3NOWNaZc2ymDU11L8tN/z94u6N0ySR8uKhVRExoPFxXa1IwezC2DKKBqaUPhaLLR4KGWtHsx5YZII5Agc75zchhL6pD7jVKwiqZaDZlQ2Gx2aVsYz12aKVnmwxOPR3p72AXU3ke1gdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = 'efdfb084093043e5c501fd0622a52319';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -1313,236 +1313,225 @@ if($message['type']=='text') {
             )
         );
     } else if ($command == '/bantuan') {
+                    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+          array (
+  'type' => 'template',
+  'altText' => 'Anda di sebut',
+  'template' =>
+  array (
+    'type' => 'carousel',
+    'columns' =>
+    array (
+      0 =>
+      array (
+        'thumbnailImageUrl' => 'https://image.freepik.com/icones-gratis/relogios-de-parede-com-horas_318-32867.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Zona waktu',
+        'text' => 'Informasi waktu di setiap kota yang ingin kamu cari',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/waktu jakarta',
+          ),
+        ),
+      ),
+      1 =>
+      array (
+        'thumbnailImageUrl' => 'https://1c7qp243xy9g1qeffp1k1nvo-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/instagram_logo.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Instagram',
+        'text' => 'Informasi akun instagram yang ingin kamu cari',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/instagram kamu',
+          ),
+        ),
+      ),
+      2 =>
+      array (
+        'thumbnailImageUrl' => 'https://unnecessaryexclamationmark.files.wordpress.com/2016/05/myanimelist-logo.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Anime',
+        'text' => 'Info anime yang ingin kamu cari',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/anime onepiece',
+          ),
+        ),
+      ),
+      3 =>
+      array (
+        'thumbnailImageUrl' => 'https://is3-ssl.mzstatic.com/image/thumb/Purple62/v4/cc/68/6c/cc686c29-ffd2-5115-2b97-c4821b548fe3/AppIcon-1x_U007emarketing-85-220-6.png/246x0w.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Praytime',
+        'text' => 'Info jadwal shalat sesuai dengan yang di cari',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/shalat jakarta',
+          ),
+        ),
+      ),
+      4 =>
+      array (
+        'thumbnailImageUrl' => 'https://i.pinimg.com/originals/d7/d8/a5/d7d8a5c1017dff37a359c95e88e0897b.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Fansign',
+        'text' => 'Text yang di tulis d kertas',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/fansign saya',
+          ),
+        ),
+      ),
+      5 =>
+      array (
+        'thumbnailImageUrl' => 'https://png.pngtree.com/element_origin_min_pic/16/10/19/1758073fffac5db.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Picture',
+        'text' => 'Pencarian gambar sesuai dengan yg kamu mau',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/gambar kucing',
+          ),
+        ),
+      ),
+      6 =>
+      array (
+        'thumbnailImageUrl' => 'https://st3.depositphotos.com/3921439/12696/v/950/depositphotos_126961774-stock-illustration-the-tv-icon-television-and.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Television',
+        'text' => 'Info jadwal TV sesuai dengan yang di cari',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/jadwaltv globaltv',
+          ),
+        ),
+      ),
+      7 =>
+      array (
+        'thumbnailImageUrl' => 'https://i.ytimg.com/vi/3hz1e4d0f9I/maxresdefault.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Music',
+        'text' => 'Info music sesuai dengan yang di cari',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/musik amy diamond heartbeat',
+          ),
+        ),
+      ),
+      8 =>
+      array (
+        'thumbnailImageUrl' => 'https://4vector.com/i/free-vector-cartoon-weather-icon-05-vector_018885_cartoon_weather_icon_05_vector.jpg',
+        'imageBackgroundColor' => '#00FFFF',
+        'title' => 'Weather',
+        'text' => 'Info cuaca sesuai dgn yang di cari',
+        'defaultAction' =>
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/222',
+        ),
+        'actions' =>
+        array (
+          0 =>
+          array (
+            'type' => 'message',
+            'label' => 'Example',
+            'text' => '/cuaca jakarta',
+          ),
+        ),
+      ),
+    ),
+    'imageAspectRatio' => 'rectangle',
+    'imageSize' => 'cover',
+  ),
+)
 
-	        $balas = array(
-							'replyToken' => $replyToken,
-							'messages' => array(
-								array (
-										  'type' => 'template',
-										  'altText' => 'Silahkan Pilih Keyword Yang Anda Inginkan',
-										  'template' => 
-										  array (
-										    'type' => 'carousel',
-										    'columns' => 
-										    array (
-										      0 => 
-										      array (
-										        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/TobyGaming74/TobyBotOa/master/Toby.png',
-										        'title' => 'Keyword 1',
-										        'text' => 'Silahkan Dipilih',
-										        'actions' => 
-										        array (
-										          0 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Anime',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /anime [Judul Anime]'
-										          ),
-										          1 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Sinopsis Anime',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /anime-syn [Judul Anime]'
-												  ),
-										          2 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Manga',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /manga [Judul Manga]'
-										          ),
-										        ),
-										      ),
-										      1 => 
-										      array (
-										        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/TobyGaming74/TobyBotOa/master/Toby.png',
-										        'title' => 'Keyword 2',
-										        'text' => 'Silahkan Dipilih',
-										        'actions' => 
-										        array (
-										          0 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Sinopsis Manga',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /manga-syn [Judul Manga]'
-										          ),
-										          1 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Film',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /film [Judul Film]'
-										          ),
-										          2 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Sinopsis Film',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /film-syn [Judul Film]'
-										          ),
-										        ),
-										      ),
-										      2 => 
-										      array (
-										        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/TobyGaming74/TobyBotOa/master/Toby.png',
-										        'title' => 'Keyword 3',
-										        'text' => 'Silahkan Dipilih',
-										        'actions' => 
-										        array (
-										          0 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Aplikasi',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /playstore [Nama Aplikasi]'
-										          ),
-										          1 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Informasi',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /myinfo'
-										          ),
-										          2 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Zodiak',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /zodiak [Tanggal Lahir]'
-										          ),
-										        ),
-										      ),
-										      3 => 
-										      array (
-										        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/TobyGaming74/TobyBotOa/master/Toby.png',
-										        'title' => 'Keyword 4',
-										        'text' => 'Silahkan Dipilih',
-										        'actions' => 
-										        array (
-										          0 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Music',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /music [Judul Lagu]'
-										          ),
-										          1 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Lirik',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /lirik [Judul Lagu]'
-										          ),
-										          2 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Waktu',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /time [Nama Kota]'
-										          ),
-										        ),
-										      ),
-										      4 => 
-										      array (
-										        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/TobyGaming74/TobyBotOa/master/Toby.png',
-										        'title' => 'Keyword 5',
-										        'text' => 'Silahkan Dipilih',
-										        'actions' => 
-										        array (
-										          0 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Lokasi',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /lokasi [Nama Kota]'
-										          ),
-										          1 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Kalender',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /kalender [Nama Kota]'
-										          ),
-										          2 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari KosaKata',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /def [Kata]'
-										          ),
-										        ),
-										      ),
-										      5 => 
-										      array (
-										        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/TobyGaming74/TobyBotOa/master/Toby.png',
-										        'title' => 'Keyword 6',
-										        'text' => 'Silahkan Dipilih',
-										        'actions' => 
-										        array (
-										          0 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Cari Qiblat',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /qiblat [Nama Kota]'
-										          ),
-										          1 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Jadwal Shalat',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /shalat [Nama Kota]'
-										          ),
-										          2 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Cari Cuaca',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /cuaca [Nama Kota]'
-										          ),
-										        ),
-										      ),
-										      6 => 
-										      array (
-										        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/TobyGaming74/TobyBotOa/master/Toby.png',
-										        'title' => 'Keyword 7',
-										        'text' => 'Silahkan Dipilih',
-										        'actions' => 
-										        array (
-										          0 => 
-										          array (
-										            'type' => 'postback',
-										            'label' => 'Convert',
-										            'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /convert [Link]'
-										          ),
-										          1 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'About',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /about'
-										          ),
-										          2 => 
-										          array (
-													'type' => 'postback',
-													'label' => 'Creator',
-													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /translate'
-										          ),
-										        ),
-										      ),											  
-										    ),
-										  ),
-										)					
-			 
-        )
-    );
-	}
-	
+
+
+)
+);
 }
 if (isset($balas)) {
     $result = json_encode($balas);
