69d98044b25c5998d99c553da10c8fb8b88ac121
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2018-04-09 07:45:58 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -642,17 +642,11 @@ if($msg_type == 'text'){
     if($filter[0] == 'apakah') {
         $balas = send(jawabs(), $replyToken);
     } else {}
-}if($msg_type == 'text'){
+} if($msg_type == 'text'){
     $pesan_datang = strtolower($message['text']);
     $filter = explode(' ', $pesan_datang);
     if($filter[0] == 'Apakah') {
         $balas = send(jawabs(), $replyToken);
-    } else {}
-} if($msg_type == 'text'){
-    $pesan_datang = strtolower($message['text']);
-    $filter = explode(' ', $pesan_datang);
-    if($filter[0] == 'bisakah') {
-        $balas = send(bisa(), $replyToken);
     } else {}
 } if($msg_type == 'text'){
     $pesan_datang = strtolower($message['text']);
